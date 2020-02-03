ca607908063d35a9a6fa58beeb15dbe8978a026e
Brunovsky <elbrunovsky@gmail.com>
2020-02-03 18:42:01 +0000

    Add nord script palette

---
+++ .config/Xthemes/nord.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# polar night, shades of black
+export nord0='#2e3440'
+export nord1='#3b4252'
+export nord2='#434c5e'
+export nord3='#4c566a'
+export nord_night0=$nord0
+export nord_night1=$nord1
+export nord_night2=$nord2
+export nord_night3=$nord3
+export nord_nightz=$nord3
+export nord_black=$nord0
+
+# snow storm, shades of white
+export nord4='#d8dee9'
+export nord5='#e5e9f0'
+export nord6='#eceff4'
+export nord_snow0=$nord4
+export nord_snow1=$nord5
+export nord_snow2=$nord6
+export nord_snowz=$nord6
+export nord_white=$nord6
+
+# frost, shades of blue
+export nord7='#8fbcbb'
+export nord8='#88c0d0'
+export nord9='#81a1c1'
+export nord10='#5e81ac'
+export nord_frost0=$nord7
+export nord_frost1=$nord8
+export nord_frost2=$nord9
+export nord_frost3=$nord10
+export nord_frostz=$nord10
+export nord_cyan=$nord_frost1
+
+# aurora, other colors
+export nord11='#bf616a'
+export nord12='#d08770'
+export nord13='#ebcb8b'
+export nord14='#a3be8c'
+export nord15='#b48ead'
+export nord_red=$nord11
+export nord_orange=$nord12
+export nord_yellow=$nord13
+export nord_green=$nord14
+export nord_purple=$nord15
+export nord_brown=$nord12
